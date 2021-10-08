bc5b21b17223c4f7c9fc29d18f1bd617e9ca59d4
Visual Student <visualstudent@zschie.edu.pl>
2021-10-08 09:25:33 +0200

    Labels, text boxes, date picker

--- Queues/Form1.Designer.cs
+++ Queues/Form1.Designer.cs
@@ -32,6 +32,9 @@ namespace Queues
             this.Name = new System.Windows.Forms.Label();
             this.AppointmentDate = new System.Windows.Forms.Label();
             this.AppointmentName = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.AppointmentNameTextBox = new System.Windows.Forms.TextBox();
+            this.AppointmentDatePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // Name
@@ -46,7 +49,7 @@ namespace Queues
             // AppointmentDate
             // 
             this.AppointmentDate.AutoSize = true;
-            this.AppointmentDate.Location = new System.Drawing.Point(15, 40);
+            this.AppointmentDate.Location = new System.Drawing.Point(15, 65);
             this.AppointmentDate.Name = "AppointmentDate";
             this.AppointmentDate.Size = new System.Drawing.Size(30, 13);
             this.AppointmentDate.TabIndex = 1;
@@ -55,17 +58,41 @@ namespace Queues
             // AppointmentName
             // 
             this.AppointmentName.AutoSize = true;
-            this.AppointmentName.Location = new System.Drawing.Point(15, 65);
+            this.AppointmentName.Location = new System.Drawing.Point(15, 40);
             this.AppointmentName.Name = "AppointmentName";
-            this.AppointmentName.Size = new System.Drawing.Size(90, 13);
+            this.AppointmentName.Size = new System.Drawing.Size(66, 13);
             this.AppointmentName.TabIndex = 3;
-            this.AppointmentName.Text = "Appointment date";
+            this.AppointmentName.Text = "Appointment";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(125, 8);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextBox.TabIndex = 4;
+            // 
+            // AppointmentNameTextBox
+            // 
+            this.AppointmentNameTextBox.Location = new System.Drawing.Point(125, 35);
+            this.AppointmentNameTextBox.Name = "AppointmentNameTextBox";
+            this.AppointmentNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AppointmentNameTextBox.TabIndex = 5;
+            // 
+            // AppointmentDatePicker
+            // 
+            this.AppointmentDatePicker.Location = new System.Drawing.Point(125, 62);
+            this.AppointmentDatePicker.Name = "AppointmentDatePicker";
+            this.AppointmentDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.AppointmentDatePicker.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.AppointmentDatePicker);
+            this.Controls.Add(this.AppointmentNameTextBox);
+            this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.AppointmentName);
             this.Controls.Add(this.AppointmentDate);
             this.Controls.Add(this.Name);
@@ -81,6 +108,9 @@ namespace Queues
         private System.Windows.Forms.Label Name;
         private System.Windows.Forms.Label AppointmentDate;
         private System.Windows.Forms.Label AppointmentName;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox AppointmentNameTextBox;
+        private System.Windows.Forms.DateTimePicker AppointmentDatePicker;
     }
 }
 
