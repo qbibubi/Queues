771d2fc986335f9279a369e07728c78d80e0b51a
Visual Student <visualstudent@zschie.edu.pl>
2021-10-08 09:34:21 +0200

    Fixed layout

--- Queues/Form1.Designer.cs
+++ Queues/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Queues
 {
-    partial class Form1
+    partial class Appointments
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -29,22 +29,13 @@ namespace Queues
         /// </summary>
         private void InitializeComponent()
         {
-            this.Name = new System.Windows.Forms.Label();
             this.AppointmentDate = new System.Windows.Forms.Label();
             this.AppointmentName = new System.Windows.Forms.Label();
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.AppointmentNameTextBox = new System.Windows.Forms.TextBox();
             this.AppointmentDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.PatientName = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // Name
-            // 
-            this.Name.AutoSize = true;
-            this.Name.Location = new System.Drawing.Point(15, 15);
-            this.Name.Name = "Name";
-            this.Name.Size = new System.Drawing.Size(35, 13);
-            this.Name.TabIndex = 0;
-            this.Name.Text = "Name";
             // 
             // AppointmentDate
             // 
@@ -66,37 +57,46 @@ namespace Queues
             // 
             // NameTextBox
             // 
-            this.NameTextBox.Location = new System.Drawing.Point(125, 8);
+            this.NameTextBox.Location = new System.Drawing.Point(90, 8);
             this.NameTextBox.Name = "NameTextBox";
             this.NameTextBox.Size = new System.Drawing.Size(100, 20);
             this.NameTextBox.TabIndex = 4;
             // 
             // AppointmentNameTextBox
             // 
-            this.AppointmentNameTextBox.Location = new System.Drawing.Point(125, 35);
+            this.AppointmentNameTextBox.Location = new System.Drawing.Point(90, 35);
             this.AppointmentNameTextBox.Name = "AppointmentNameTextBox";
             this.AppointmentNameTextBox.Size = new System.Drawing.Size(100, 20);
             this.AppointmentNameTextBox.TabIndex = 5;
             // 
             // AppointmentDatePicker
             // 
-            this.AppointmentDatePicker.Location = new System.Drawing.Point(125, 62);
+            this.AppointmentDatePicker.Location = new System.Drawing.Point(90, 62);
             this.AppointmentDatePicker.Name = "AppointmentDatePicker";
             this.AppointmentDatePicker.Size = new System.Drawing.Size(200, 20);
             this.AppointmentDatePicker.TabIndex = 6;
             // 
-            // Form1
+            // PatientName
+            // 
+            this.PatientName.AutoSize = true;
+            this.PatientName.Location = new System.Drawing.Point(15, 15);
+            this.PatientName.Name = "PatientName";
+            this.PatientName.Size = new System.Drawing.Size(69, 13);
+            this.PatientName.TabIndex = 7;
+            this.PatientName.Text = "Patient name";
+            // 
+            // Appointments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.PatientName);
             this.Controls.Add(this.AppointmentDatePicker);
             this.Controls.Add(this.AppointmentNameTextBox);
             this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.AppointmentName);
             this.Controls.Add(this.AppointmentDate);
-            this.Controls.Add(this.Name);
-            this.Name = "Form1";
+            this.Name = "Appointments";
             this.Text = "Appointments";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -105,12 +105,12 @@ namespace Queues
 
         #endregion
 
-        private System.Windows.Forms.Label Name;
         private System.Windows.Forms.Label AppointmentDate;
         private System.Windows.Forms.Label AppointmentName;
         private System.Windows.Forms.TextBox NameTextBox;
         private System.Windows.Forms.TextBox AppointmentNameTextBox;
         private System.Windows.Forms.DateTimePicker AppointmentDatePicker;
+        private System.Windows.Forms.Label PatientName;
     }
 }
 
--- Queues/Form1.cs
+++ Queues/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Queues
 {
-    public partial class Form1 : Form
+    public partial class Appointments : Form
     {
-        public Form1()
+        public Appointments()
         {
             InitializeComponent();
         }
