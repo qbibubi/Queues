bc122ab363311c441fdc79fe7f1b159757eedde3
Visual Student <visualstudent@zschie.edu.pl>
2021-10-08 10:03:50 +0200

    Labels, buttons

--- Queues/Form1.Designer.cs
+++ Queues/Form1.Designer.cs
@@ -29,12 +29,21 @@ namespace Queues
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.AppointmentDate = new System.Windows.Forms.Label();
             this.AppointmentName = new System.Windows.Forms.Label();
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.AppointmentNameTextBox = new System.Windows.Forms.TextBox();
             this.AppointmentDatePicker = new System.Windows.Forms.DateTimePicker();
             this.PatientName = new System.Windows.Forms.Label();
+            this.CurrentDate = new System.Windows.Forms.Label();
+            this.TimeManager = new System.Windows.Forms.Timer(this.components);
+            this.TextCurrentDate = new System.Windows.Forms.Label();
+            this.StartQueue = new System.Windows.Forms.Button();
+            this.StopQueue = new System.Windows.Forms.Button();
+            this.AddToQueue = new System.Windows.Forms.Button();
+            this.RemoveFromQueue = new System.Windows.Forms.Button();
+            this.InsertIntoQueue = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // AppointmentDate
@@ -64,7 +73,7 @@ namespace Queues
             // 
             // AppointmentNameTextBox
             // 
-            this.AppointmentNameTextBox.Location = new System.Drawing.Point(90, 35);
+            this.AppointmentNameTextBox.Location = new System.Drawing.Point(90, 33);
             this.AppointmentNameTextBox.Name = "AppointmentNameTextBox";
             this.AppointmentNameTextBox.Size = new System.Drawing.Size(100, 20);
             this.AppointmentNameTextBox.TabIndex = 5;
@@ -72,7 +81,7 @@ namespace Queues
             // AppointmentDatePicker
             // 
             this.AppointmentDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.AppointmentDatePicker.Location = new System.Drawing.Point(90, 62);
+            this.AppointmentDatePicker.Location = new System.Drawing.Point(90, 58);
             this.AppointmentDatePicker.Name = "AppointmentDatePicker";
             this.AppointmentDatePicker.Size = new System.Drawing.Size(100, 20);
             this.AppointmentDatePicker.TabIndex = 6;
@@ -86,11 +95,81 @@ namespace Queues
             this.PatientName.TabIndex = 7;
             this.PatientName.Text = "Patient name";
             // 
+            // CurrentDate
+            // 
+            this.CurrentDate.AutoSize = true;
+            this.CurrentDate.Location = new System.Drawing.Point(15, 90);
+            this.CurrentDate.Name = "CurrentDate";
+            this.CurrentDate.Size = new System.Drawing.Size(65, 13);
+            this.CurrentDate.TabIndex = 8;
+            this.CurrentDate.Text = "Current date";
+            // 
+            // TextCurrentDate
+            // 
+            this.TextCurrentDate.AutoSize = true;
+            this.TextCurrentDate.Location = new System.Drawing.Point(90, 90);
+            this.TextCurrentDate.Name = "TextCurrentDate";
+            this.TextCurrentDate.Size = new System.Drawing.Size(64, 13);
+            this.TextCurrentDate.TabIndex = 9;
+            this.TextCurrentDate.Text = "placeHolder";
+            // 
+            // StartQueue
+            // 
+            this.StartQueue.Location = new System.Drawing.Point(200, 7);
+            this.StartQueue.Name = "StartQueue";
+            this.StartQueue.Size = new System.Drawing.Size(100, 20);
+            this.StartQueue.TabIndex = 10;
+            this.StartQueue.Text = "Start queue";
+            this.StartQueue.UseVisualStyleBackColor = true;
+            // 
+            // StopQueue
+            // 
+            this.StopQueue.Location = new System.Drawing.Point(310, 7);
+            this.StopQueue.Name = "StopQueue";
+            this.StopQueue.Size = new System.Drawing.Size(100, 20);
+            this.StopQueue.TabIndex = 11;
+            this.StopQueue.Text = "Stop queue";
+            this.StopQueue.UseVisualStyleBackColor = true;
+            // 
+            // AddToQueue
+            // 
+            this.AddToQueue.Location = new System.Drawing.Point(200, 32);
+            this.AddToQueue.Name = "AddToQueue";
+            this.AddToQueue.Size = new System.Drawing.Size(100, 20);
+            this.AddToQueue.TabIndex = 12;
+            this.AddToQueue.Text = "Add to queue";
+            this.AddToQueue.UseVisualStyleBackColor = true;
+            // 
+            // RemoveFromQueue
+            // 
+            this.RemoveFromQueue.Location = new System.Drawing.Point(310, 32);
+            this.RemoveFromQueue.Name = "RemoveFromQueue";
+            this.RemoveFromQueue.Size = new System.Drawing.Size(100, 20);
+            this.RemoveFromQueue.TabIndex = 13;
+            this.RemoveFromQueue.Text = "Rem. from queue";
+            this.RemoveFromQueue.UseVisualStyleBackColor = true;
+            // 
+            // InsertIntoQueue
+            // 
+            this.InsertIntoQueue.Location = new System.Drawing.Point(200, 58);
+            this.InsertIntoQueue.Name = "InsertIntoQueue";
+            this.InsertIntoQueue.Size = new System.Drawing.Size(100, 20);
+            this.InsertIntoQueue.TabIndex = 14;
+            this.InsertIntoQueue.Text = "Insert into queue";
+            this.InsertIntoQueue.UseVisualStyleBackColor = true;
+            // 
             // Appointments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.ClientSize = new System.Drawing.Size(424, 311);
+            this.Controls.Add(this.InsertIntoQueue);
+            this.Controls.Add(this.RemoveFromQueue);
+            this.Controls.Add(this.AddToQueue);
+            this.Controls.Add(this.StopQueue);
+            this.Controls.Add(this.StartQueue);
+            this.Controls.Add(this.TextCurrentDate);
+            this.Controls.Add(this.CurrentDate);
             this.Controls.Add(this.PatientName);
             this.Controls.Add(this.AppointmentDatePicker);
             this.Controls.Add(this.AppointmentNameTextBox);
@@ -112,6 +191,14 @@ namespace Queues
         private System.Windows.Forms.TextBox AppointmentNameTextBox;
         private System.Windows.Forms.DateTimePicker AppointmentDatePicker;
         private System.Windows.Forms.Label PatientName;
+        private System.Windows.Forms.Label CurrentDate;
+        private System.Windows.Forms.Timer TimeManager;
+        private System.Windows.Forms.Label TextCurrentDate;
+        private System.Windows.Forms.Button StartQueue;
+        private System.Windows.Forms.Button StopQueue;
+        private System.Windows.Forms.Button AddToQueue;
+        private System.Windows.Forms.Button RemoveFromQueue;
+        private System.Windows.Forms.Button InsertIntoQueue;
     }
 }
 
