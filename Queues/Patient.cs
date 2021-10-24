8b00a437222416edc034120c9a4d811019319a54
Qbibubi <58397100+Nantathu@users.noreply.github.com>
2021-10-24 09:51:44 +0200

    Patient class

---
+++ Queues/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Patient
+    {
+        private string patient_name, patient_appointment;
+        private DateTime patient_appointment_date;
+
+        public DateTime GetPatientAppointmentDate() { return patient_appointment_date; }
+        public string GetPatientAppointment()       { return patient_appointment; }
+        public string GetPatientName()              { return patient_name; }
+
+        public void SetValues(string p_name, string p_appointment, DateTime p_app_date)
+        {
+            patient_name = p_name;
+            patient_appointment = p_appointment;
+            patient_appointment_date = p_app_date;
+        }
+    }
+}
